23ca9302454009c73cad82120a5c6a12be046260
Muhammad Ahmad <ahmadmian257@gmail.com>
2025-01-16 11:39:17 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import './style.css'
+
+const root = ReactDOM.createRoot(document.getElementById('r'))
+
+root.render(
+  
+
+  <div className=''> 
+    <App />
+
+
+  </div>
+
+
+);
